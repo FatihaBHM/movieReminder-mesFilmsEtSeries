5ca3b79c39d263194174f20b37d81554ca46d441
user_name <example.gmail.com>
2024-03-13 22:48:13 +0100

    :sparkles: feat: Mise à jour de l'infrastructure de l'application
    Mise à jour de la configuration de Docker, amélioration de l'organisation du code, mise à jour des dépendances Maven, amélioration de l'application.properties et ajout d'instructions pour Docker dans README.md.

--- exposition/src/main/java/org/lafabrique_epita/exposition/ExpositionApplication.java
+++ exposition/src/main/java/org/lafabrique_epita/exposition/ExpositionApplication.java
@@ -2,8 +2,10 @@ package org.lafabrique_epita.exposition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.lafabrique_epita.Domain")
 public class ExpositionApplication {
 
     public static void main(String[] args) {
